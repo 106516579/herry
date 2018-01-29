6dd3191c5c705ad715a00b39a1616f039ab91256
106516579 <106516579@qq.com>
2018-01-29 15:01:03 +0800

    zhaopin

---
+++ zhaopin/js/jqnav.js
@@ -0,0 +1,22 @@
+﻿$(document).ready(function(){
+	maphover();
+	$(".city").find("a").click(function(){
+		$(".informations").find("."+this.dataset.city).css("display","block").siblings().css("display","none");
+	});
+});
+
+function maphover(){
+	var self = "";
+	$(".city").hover(
+		function(){
+			self = $(this);
+			self.addClass("hover").children("div").show();
+		},
+		function(){
+			self = $(this);
+			self.children("div").hide();
+			self.removeClass("hover");
+		}
+	);	
+};
+//.ct.bg
